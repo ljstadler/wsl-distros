1c079a44e5878c5c8d7c2caf001abd9fa07612d1
Lohan Stadler <lohanjs@outlook.com>
2024-03-02 09:26:04 +0200

    Add optional setup

--- alpine-wsl.ps1
+++ alpine-wsl.ps1
@@ -8,3 +8,28 @@ New-Item -Path "$env:LOCALAPPDATA\Packages\alpine-wsl" -ItemType "directory";
 wsl --import Alpine "$env:LOCALAPPDATA\Packages\alpine-wsl" "$HOME\Downloads\alpine.tar.gz";
 
 Remove-Item "$HOME\Downloads\alpine.tar.gz";
+
+do {
+    $setup = Read-Host "Perform Setup? [y/n]"
+    if ($setup -eq "y") {
+        $username = Read-Host "Username"
+        do {
+            $password = Read-Host "Password" -MaskInput
+            $rpassword = Read-Host "Retype Password" -MaskInput
+        } until ($password -eq $rpassword)
+        wsl -d Alpine -e ash -c @"
+            apk upgrade -U &&
+            apk add alpine-base doas &&
+            adduser -D -G wheel ${username} &&
+            echo '${username}:${password}' | chpasswd &&
+            echo 'permit persist :wheel' > /etc/doas.d/doas.conf &&
+            echo 'iface default inet dhcp' > /etc/network/interfaces &&
+            echo -e '[boot]\ncommand="/sbin/openrc default"\n\n[user]\ndefault=${username}' > /etc/wsl.conf 
+"@;
+        break
+    }
+} until ($setup -eq "n")
+
+wsl -t Alpine;
+
+wsl -d Alpine --cd ~;
